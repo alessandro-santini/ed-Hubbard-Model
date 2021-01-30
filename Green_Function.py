8c8f225d6d96099b3f65fc35a3bcd06838735bea
Alessandro Santini <76155864+alessandro-santini@users.noreply.github.com>
2021-01-30 12:25:48 +0100

    Add files via upload

---
+++ Green_Function.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 30 11:30:51 2021
+
+@author: asant
+"""
+
+import HubbardModelTools as hm
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+from   scipy import interpolate
+
+def c(){
+        
+        }
+
+
+hf = hm.FermionicBasis_1d(5, 5, 10)
+U  = 5.
+H  = hm.H_Qx(hf,0.,U) 
+dimH = H.shape[0]
+v0 = np.random.random(dimH)+1j*np.random.random(dimH-)
+states, eig, _, _ = hm.Lanczos(H,v0,100,m=3)
+gs_energy = eig[0]
+gs_state  = states[:,0]
+#%%
+hf_minus = hm.FermionicBasis_1d(5,4,10)
+hf_minus.set_RepQx(0.)

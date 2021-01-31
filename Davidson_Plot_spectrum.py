0cf5ec543d1bec0eb5c37ca41cc974e922b53ee3
Alessandro Santini <76155864+Alessandro-Santini@users.noreply.github.com>
2021-01-31 14:24:41 +0100

    added fancy plots files

--- Davidson_Plot_spectrum.py
+++ Davidson_Plot_spectrum.py
@@ -14,46 +14,42 @@ import seaborn as sns
 from scipy import interpolate
 # from scipy.sparse.linalg import eigsh
 
-#plt.close('all')
+plt.close('all')
 hf = hm.FermionicBasis_1d(5, 5, 10)
 
 U  = 5.0
 t1 = time.perf_counter()
-m  = 0
-min_qL = np.zeros_like(hf.momenta)
+m  = 5
+
+Dav_spectrum = np.zeros((hf.momenta.size,m))
 
 for i,qx in enumerate(hf.momenta):
   H = hm.H_Qx(hf, qx, U)
-  dimH = H.shape[0]
-  v0   = np.random.random(dimH)+1j*np.random.random(dimH)
-  states, eig, Ndone, eps = hm.Lanczos(H.tocsr(),v0, 50, m)
-  min_qL[i] = min(eig)
+  states, eig, Ndone, eps = hm.Davidson(H.tocsr(), 1000, m)
+  Dav_spectrum[i, :] = eig[:m]
 
 t2 = time.perf_counter()
 print(f"Exact diagonalization in {t2-t1}s")
-print(f"Ground state energy: { min_qL.min() }")
+print(f"Ground state energy: { np.min(Dav_spectrum) }")
+
+plt.figure(figsize=(14,7))
+plt.gca().xaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
 
 plt.rc('text',usetex=True)
 cpalette = sns.color_palette("icefire",n_colors = len(hf.momenta)+1)
 
-# Just for symmetry
 plot_momenta = hf.momenta.copy()
 plot_momenta = np.append(plot_momenta, -hf.momenta[0])
-
-plt.figure(figsize=(14,7))
-plt.gca().xaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
+q_TL     = np.linspace(plot_momenta[0],plot_momenta[-1],1000)
 
 plt.xlabel(r"$q$", fontsize = 26)
 plt.ylabel(r"$\min E_q$", fontsize = 26)
 plt.gca().tick_params(axis='both', which='major', labelsize=14)
 
-min_qL = np.append(min_qL, min_qL[0])
-
-# Smooth-line in the Thermodynamic Limit
-min_qL_TL = interpolate.interp1d(plot_momenta, min_qL,kind='quadratic')
-q_TL     = np.linspace(plot_momenta[0],plot_momenta[-1],1000)
-
-plt.plot(q_TL, min_qL_TL(q_TL), '--', color = 'k', linewidth=0.75,zorder=1)
-plt.scatter(plot_momenta, min_qL, c=cpalette, s=60,zorder=2)
-plt.xticks(plot_momenta)
-
+for energies in Dav_spectrum.T:    
+    en_plot = np.append(energies, energies[0])
+    # Smooth-line in the Thermodynamic Limit
+    en_plot_TL = interpolate.interp1d(plot_momenta, en_plot,kind='quadratic')
+    plt.plot(q_TL, en_plot_TL(q_TL), '--', color = 'k', linewidth=0.75,zorder=1)
+    plt.scatter(plot_momenta, en_plot, c=cpalette, s=50,zorder=2)
+    plt.xticks(plot_momenta)

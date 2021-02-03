1f5bb4b5a6f673d2a460f534167edcb9a7addd6c
SamueleGiuli <78253684+SamueleGiuli@users.noreply.github.com>
2021-02-03 17:55:08 +0100

    Add files via upload
    A new loop

--- Green_Function.py
+++ Green_Function.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 from   scipy import interpolate
 import scipy.linalg as sl
+from scipy.signal import find_peaks
 
 def c(s, i):
     lst = list(s)
@@ -22,7 +23,7 @@ def c_q_up(basis,basis_minus,state,qx,k):
                 if(Up_state[i] == '1'):
                     NewUpInt = int(c(Up_state,i), 2)
                     Swapped_rep, j_x, sign, info = basis_minus.check_rep(NewUpInt, rep[1])
-                    sign = sign*(-1)**np.binary_repr(NewUpInt,width = basis.L)[:i].count('1')
+                    sign = sign*(-1)**(np.binary_repr(NewUpInt,width = basis.L)[:i].count('1')+np.binary_repr(rep[1],width = basis.L)[:i].count('1'))
                     if(info):
                         Index_Swapped_rep = RepQxToIndex_minus[str(Swapped_rep[0])]
                         components[Index_Swapped_rep] += sign*np.exp( 1j*(j_x*(k-qx)-qx*i) )*\
@@ -47,98 +48,105 @@ def n_q0(basis,state):
 
 hf   = hm.FermionicBasis_1d(3, 3, 6)
 hf_minus = hm.FermionicBasis_1d(2, 3, 6)
-q    = 0.
+U    = 5.
 k    = 0.
-U    = 0.
 H    = hm.H_Qx(hf,k,U) 
-H_minus = hm.H_Qx(hf_minus,k-q,U)
 dimH = H.shape[0]
 v0   = np.random.random(dimH)+1j*np.random.random(dimH)
-states, eig, Ndone, _ = hm.Lanczos(H,v0,200,m=0)
-
+states, eig, Ndone, _ = hm.Lanczos(H,v0,100,m=0)
 gs_energy = eig[0]
-
 gs_state  = states[:,0]
 
 
-#Lanczos procedure for density Green's function
-N = len(hf_minus.RepQx)
-Psi = c_q_up(hf,hf_minus,gs_state,q,k)
-PsiMinus = np.zeros_like(Psi, dtype=np.complex128)
-PsiPlus  = np.zeros_like(Psi, dtype=np.complex128)
-
-Vm    = np.reshape(Psi.copy(),newshape=(N,1))
-alpha = np.array([])
-beta  = np.array([])
-alpha = np.append(alpha, np.vdot(Psi,H_minus.dot(Psi)) )
-beta = np.append(beta,0.0)
-
-n_lanc = 20
-for i in np.arange(1,n_lanc):
-
-    PsiPlus  = (H_minus.dot(Psi)-alpha[i-1]*Psi)-beta[i-1]*PsiMinus
-    beta     = np.append(beta,np.linalg.norm(PsiPlus))
-    PsiPlus  = PsiPlus/beta[i]
-    Vm       = np.append(Vm,np.reshape(PsiPlus,newshape=(N,1) ),axis=1)
-    PsiMinus = Psi.copy()
-    Psi      = PsiPlus.copy()
-    
-    alpha  = np.append(alpha, np.vdot(Psi,H_minus.dot(Psi)) )
-
-u = np.zeros(shape=(n_lanc,1),dtype=np.float64)
-u[0,0]=1.
-
-
-n_g = 500
+n_g = 20000
 G = np.zeros(n_g)
-zspace=np.linspace(0,10,n_g)
-for iz,z in enumerate(zspace):
-    m = np.diag(z-alpha, k=0)-np.diag(beta[1:],k=1)-np.diag(beta[1:],k=-1) 
-    num = np.linalg.det( np.append(u,m[:,1:],axis=1) )
-    den = np.linalg.det(m)
-    G[iz] = (num/den).imag
+zspace=np.linspace(gs_energy,17+gs_energy,n_g)
+epsi=1j*1e-10
 
-plt.plot(zspace,G)
+for iii in range(6):
+    q    = hf.momenta[iii]
+    H_minus = hm.H_Qx(hf_minus,k-q,U)
+
+    #Lanczos procedure for density Green's function
+    N = len(hf_minus.RepQx)
+    Psi = c_q_up(hf,hf_minus,gs_state,q,k)
+    PsiMinus = np.zeros_like(Psi, dtype=np.complex128)
+    PsiPlus  = np.zeros_like(Psi, dtype=np.complex128)
+
+    Vm    = np.reshape(Psi.copy(),newshape=(N,1))
+    alpha = np.array([])
+    beta  = np.array([])
+    alpha = np.append(alpha, np.vdot(Psi,H_minus.dot(Psi)) )
+    beta  = np.append(beta,0.0)
+
+    n_lanc = 100
+    for i in np.arange(1,n_lanc):
+
+        PsiPlus  = (H_minus.dot(Psi)-alpha[i-1]*Psi)-beta[i-1]*PsiMinus
+        beta     = np.append(beta,np.linalg.norm(PsiPlus))
+        PsiPlus  = PsiPlus/beta[i]
+        Vm       = np.append(Vm,np.reshape(PsiPlus,newshape=(N,1) ),axis=1)
+        PsiMinus = Psi.copy()
+        Psi      = PsiPlus.copy()
+        
+        alpha  = np.append(alpha, np.vdot(Psi,H_minus.dot(Psi)) )
+
+    u = np.zeros(shape=(n_lanc,1),dtype=np.complex128)
+    u[0,0]=1.
+
+    #print(gs_energy,np.linalg.eigh(np.diag(alpha, k=0)+np.diag(beta[1:],k=1)+np.diag(beta[1:],k=-1))[0][0])
+
+
+    for iz,z in enumerate(zspace):
+        m = np.diag(z+epsi-alpha, k=0)-np.diag(beta[1:],k=1)-np.diag(beta[1:],k=-1) 
+        num = np.linalg.det( np.append(u,m[:,1:],axis=1) )
+        den = np.linalg.det(m)
+        G[iz] += (num/den).imag
+
+
+    print(zspace[find_peaks(G)[0]])
+plt.plot(zspace,abs(G))
+plt.yscale('log')
 plt.show()
 
 
 
 
 """
-#Lanczos procedure for density Green's function
-N = len(hf.RepQx)
-Psi = n_q0(hf,gs_state)
-PsiMinus = np.zeros_like(Psi, dtype=np.complex128)
-PsiPlus  = np.zeros_like(Psi, dtype=np.complex128)
+    #Lanczos procedure for density Green's function
+    N = len(hf.RepQx)
+    Psi = n_q0(hf,gs_state)
+    PsiMinus = np.zeros_like(Psi, dtype=np.complex128)
+    PsiPlus  = np.zeros_like(Psi, dtype=np.complex128)
 
-Vm    = np.reshape(Psi.copy(),newshape=(N,1))
-alpha = np.array([])
-beta  = np.array([])
-alpha = np.append(alpha, np.vdot(Psi,H.dot(Psi)) )
-beta = np.append(beta,0.0)
+    Vm    = np.reshape(Psi.copy(),newshape=(N,1))
+    alpha = np.array([])
+    beta  = np.array([])
+    alpha = np.append(alpha, np.vdot(Psi,H.dot(Psi)) )
+    beta = np.append(beta,0.0)
 
-for i in np.arange(1,100):
+    for i in np.arange(1,100):
 
-    PsiPlus  = (H.dot(Psi)-alpha[i-1]*Psi)-beta[i-1]*PsiMinus
-    beta     = np.append(beta,np.linalg.norm(PsiPlus))
-    PsiPlus  = PsiPlus/beta[i]
-    Vm       = np.append(Vm,np.reshape(PsiPlus,newshape=(N,1) ),axis=1)
-    PsiMinus = Psi.copy()
-    Psi      = PsiPlus.copy()
+        PsiPlus  = (H.dot(Psi)-alpha[i-1]*Psi)-beta[i-1]*PsiMinus
+        beta     = np.append(beta,np.linalg.norm(PsiPlus))
+        PsiPlus  = PsiPlus/beta[i]
+        Vm       = np.append(Vm,np.reshape(PsiPlus,newshape=(N,1) ),axis=1)
+        PsiMinus = Psi.copy()
+        Psi      = PsiPlus.copy()
 
-    alpha  = np.append(alpha, np.vdot(Psi,H.dot(Psi)) )
-    eig, s = sl.eigh_tridiagonal(alpha.real,beta[1:].real)
-u = np.zeros(shape=(100,1),dtype=np.float64)
-u[0,0]=1.
+        alpha  = np.append(alpha, np.vdot(Psi,H.dot(Psi)) )
+        eig, s = sl.eigh_tridiagonal(alpha.real,beta[1:].real)
+    u = np.zeros(shape=(100,1),dtype=np.float64)
+    u[0,0]=1.
 
-G = np.zeros(100)
-zspace=np.linspace(0,10,100)
-for iz,z in enumerate(zspace):
-    m = np.diag(z-alpha, k=0)+np.diag(beta[1:],k=1)+np.diag(beta[1:],k=-1) 
-    num = np.linalg.det( np.append(u,m[:,1:],axis=1) )
-    den = np.linalg.det(m)
-    G[iz] = (num/den).imag
+    G = np.zeros(100)
+    zspace=np.linspace(0,10,100)
+    for iz,z in enumerate(zspace):
+        m = np.diag(z-alpha, k=0)+np.diag(beta[1:],k=1)+np.diag(beta[1:],k=-1) 
+        num = np.linalg.det( np.append(u,m[:,1:],axis=1) )
+        den = np.linalg.det(m)
+        G[iz] = (num/den).imag
 
-plt.plot(zspace,G)
-plt.show()
+    plt.plot(zspace,G)
+    plt.show()
 """
